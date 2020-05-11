d6273585deddbb81c8d87d5f31ae41036d60715c
balancex <yuquanxiao@outlook.com>
2020-05-11 15:19:44 +0800

    KMP

--- 2020-05-11-KMP/src/KMP_Test.java
+++ 2020-05-11-KMP/src/KMP_Test.java
@@ -1,48 +1,3 @@
-# LeetCodeEveryday
-for interest also for job.
-(非特别说明，以下题解皆为原创)
-### 寻找数组中仅出现一次的两个数（2020-04-28）
-[leetcode题解](https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof/solution/can-zhao-yi-huo-jie-jue-fang-an-de-javaban-ben-by-/)
-
-### 查找山脉数组（2020-04-29）
-[leetcode题解](https://leetcode-cn.com/problems/find-in-mountain-array/solution/javashuang-bai-fen-bai-tong-guo-shi-jian-fu-za-du-/)
-
-### 快乐数判断（2020-04-30）
-[leetcode题解](https://leetcode-cn.com/problems/happy-number/solution/javasan-lie-biao-zuo-xun-huan-jian-ce-by-balancex-/)
-
-### 合并两个有序链表（2020-05-01）
-链表迭代中留意虚拟头节点的用法
-
-### 最长无重复子串（2020-05-05）
-双指针+散列表查重并记录索引
-
-### 最大子序列和（2020-05-05）
-DP状态选择以哪个索引结尾，dp[i] = max(dp[i-1] + nums[i], nums[i])
-
-### 最低票价（2020-05-06）
-[leetcode题解](https://leetcode-cn.com/problems/minimum-cost-for-tickets/solution/zhuang-tai-zhuan-yi-fang-cheng-zi-di-xiang-shang-s/)
-
-### 最大正方形（2020-05-08）
-[leetcode题解](https://leetcode-cn.com/problems/maximal-square/solution/san-ge-dpshu-zu-dong-tai-gui-hua-javashi-xian-by-b/)
-
-### 字典树查单词频率（2020-05-10）
-注意字典树节点的定义
-```java
-class TrieNode{
-    int cnt;
-    //只定义了26个引用
-    TrieNode[] son = new TrieNode[26];
-    //真正用到某一结点时，还需要son[i] = new TrieNode();定义实体
-    
-    public TrieNode(){
-        this.cnt = 0;    
-    }
-}
-```
-
-### KMP字符串匹配高效算法
-模式串的next表（核心）
-```java
 public class KMP_Test {
     public static void main(String[] args) {
         String pattern = new String("bc");
@@ -97,5 +52,3 @@ class KMP {
         return next;
     }
 }
-
-```
